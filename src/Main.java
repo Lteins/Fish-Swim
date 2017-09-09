ac30f726cdc6ae96d7c6057edf9e33803bfb4d2c
Sihan Liu <leo990629@gmail.com>
2017-09-09 13:23:06 -0500

    Fish Move 2.0

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,29 @@ public class Main {
 	
 	//main
 	public static void main(String args[]) {
+		// create the tank
 		char[][] tank = new char [8][32];
 		fillTank(tank, "~".charAt(0));
-		renderTank(tank);
-		int[][] positions = generateRandomPositions(3,2,5);
-		for (int i=0;i<positions.length;i++) {
-			System.out.println("{" + positions[i][0] + " , " + positions[i][1] + "}");
+		// generate fish positions
+		int[][] fishPositions = new int[4][2];
+		fishPositions = generateRandomPositions(4, tank[0].length, tank.length);
+		// repeat forever:
+		while (true){
+		    // update the simulation by:
+		    // moving all fish one position to the right
+		    moveAllFish(fishPositions, tank[0].length, tank.length);
+		    // render (draw) the simulation by:
+		    // clear the tank by filling it with water (~)
+		    fillTank(tank, "~".charAt(0));
+		    // place each fish into the tank
+		    for (int i=0; i<fishPositions.length;i++){
+		    	placeFishInTank(tank, fishPositions[i][0], fishPositions[i][1]);
+		    }
+		    // draw the contents of this tank to the screen
+		 	renderTank(tank);
+		    // pause briefly to slow down the simulation a bit
+		    Utility.pause(200);
+		    System.out.println("\n\n\n");
 		}
 	}
 	
@@ -44,17 +61,32 @@ public class Main {
 	{
 		for(int i=0;i<tank.length;i++) {
 			for (int j=0;j<tank[0].length;j++) {
-				System.out.print(tank[i][j] + " ");
+				System.out.print(tank[i][j]);
 			}
 			System.out.print("\n");
 		}	 
 	}
-	
+	/**
+	 * 
+	 * @param number: the number of random positions to generate
+	 * @param width: the range of the width(column) (should be smaller than 32)
+	 * @param height: the range of the height(row) (should be smaller than 8)
+	 * @return the random position in an array{{row, col} ...}
+	 */
 	public static int[][] generateRandomPositions(int number, int width, int height)
 	{
-		width = Math.min(width, 32);
-		height = Math.max(height, 8);
+		/* No longer needed
+		// Check the boundary of input
+		if (width>32 || width<0)
+			System.out.println("Width out of boundary");
+		if (height>8 || height<0)
+			System.out.println("Height out of boundary");
 		
+		width = Math.min(width, 32);
+		width = Math.max(width, 0);
+		height = Math.min(height, 8);
+		height = Math.max(height, 0);
+		*/
 		int [][] randomPositions = new int[number][2];
 		for (int i=0;i<number;i++) {
 			randomPositions[i][1] = Utility.randomInt(width+1);
@@ -62,4 +94,38 @@ public class Main {
 		}
 		return randomPositions;
 	}
+	
+	public static void placeFishInTank(char[][] tank, int row, int col) {
+		if (row<0 || row>=tank.length) {
+			while(row<0) {
+				row = row + tank.length;
+			}
+			row = row%tank.length;			
+		}
+
+		String fish = "><((‘>";
+		for (int i=col-5; i<=col;i++) {
+			int temp = i;
+			if (temp<0 || temp>=tank[0].length) {
+				while(temp<0) {
+					temp = temp + tank[0].length;
+				}
+				temp = temp%tank[0].length;
+			}
+//			System.out.println("row is "+ row);
+//			System.out.println("temp is "+ temp);
+//			System.out.println("i is "+ i);
+//			System.out.println("i - (col-5) is " + (i-(col-5)));
+			tank[row][temp] = fish.charAt(i-(col-5));
+		}
+	}
+
+	public static void moveAllFish(int[][] fishPositions, int width, int height){
+		for (int i=0; i<fishPositions.length;i++){
+			fishPositions[i][1] = ((fishPositions[i][1] + 1)>=width)?
+									((fishPositions[i][1]+1)%width):
+									(fishPositions[i][1]+1);
+		}
+	}
+
 }
