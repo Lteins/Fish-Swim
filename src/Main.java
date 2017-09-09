e35145528d226d307031499087d27a5609c49092
SIHAN LIU <sliu556@info-hsg-109-x.ad.wisc.edu>
2017-09-09 17:53:56 -0500

    Finish

--- src/Main.java
+++ src/Main.java
@@ -8,19 +8,38 @@ public class Main {
 		fillTank(tank, "~".charAt(0));
 		// generate fish positions
 		int[][] fishPositions = new int[4][2];
+		int[][] hookPositions = new int[1][2];
+		int[][] foodPositions = new int[6][2];
 		fishPositions = generateRandomPositions(4, tank[0].length, tank.length);
+		hookPositions = generateRandomPositions(1, tank[0].length, tank.length);
+		foodPositions = generateRandomPositions(6, tank[0].length, tank.length);
 		// repeat forever:
 		while (true){
 		    // update the simulation by:
-		    // moving all fish one position to the right
-		    moveAllFish(fishPositions, tank[0].length, tank.length);
+		    //---------------Motion Module Start--------------------
+		    //move the fish
+		    moveAllObjects(fishPositions, 1, 0, tank[0].length, tank.length);
+		    //move the hook
+		    moveAllObjects(hookPositions, 0, -1, tank[0].length, tank.length);
+		    //move the food
+		    moveAllObjects(foodPositions, 1, 1, tank[0].length, tank.length);
+		    //---------------Motion Module End----------------------
+
 		    // render (draw) the simulation by:
 		    // clear the tank by filling it with water (~)
 		    fillTank(tank, "~".charAt(0));
+		    //---------------Write new Positions into the tank--------------------
 		    // place each fish into the tank
-		    for (int i=0; i<fishPositions.length;i++){
-		    	placeFishInTank(tank, fishPositions[i][0], fishPositions[i][1]);
-		    }
+		    for (int i=0; i<fishPositions.length;i++)
+		    	placeObjectInTank("><((‘>", tank, fishPositions[i][0], fishPositions[i][1]);
+		    // place each hook into the tank
+		    for (int i=0; i<hookPositions.length;i++)
+		    		placeObjectInTank("J", tank, hookPositions[i][0], hookPositions[i][1]);
+		    // place each food into the tank
+		    for (int i=0; i<foodPositions.length;i++)
+		    	placeObjectInTank("*", tank, foodPositions[i][0], foodPositions[i][1]);
+		    //--------------------------------------------------------------------
+
 		    // draw the contents of this tank to the screen
 		 	renderTank(tank);
 		    // pause briefly to slow down the simulation a bit
@@ -89,8 +108,8 @@ public class Main {
 		*/
 		int [][] randomPositions = new int[number][2];
 		for (int i=0;i<number;i++) {
-			randomPositions[i][1] = Utility.randomInt(width+1);
-			randomPositions[i][0] = Utility.randomInt(height+1);
+			randomPositions[i][0] = Utility.randomInt(width);
+			randomPositions[i][1] = Utility.randomInt(height);
 		}
 		return randomPositions;
 	}
@@ -120,6 +139,30 @@ public class Main {
 		}
 	}
 
+	public static void placeObjectInTank(String object, char[][] tank, int column, int row) {
+		if (row<0 || row>=tank.length) {
+			while(row<0) {
+				row = row + tank.length;
+			}
+			row = row%tank.length;			
+		}
+
+		for (int i=column-object.length()+1; i<=column;i++) {
+			int temp = i;
+			if (temp<0 || temp>=tank[0].length) {
+				while(temp<0) {
+					temp = temp + tank[0].length;
+				}
+				temp = temp%tank[0].length;
+			}
+//			System.out.println("row is "+ row);
+//			System.out.println("temp is "+ temp);
+//			System.out.println("i is "+ i);
+//			System.out.println("i - (col-5) is " + (i-(col-5)));
+			tank[row][temp] = object.charAt(i-(column-object.length()+1));
+		}
+	}
+
 	public static void moveAllFish(int[][] fishPositions, int width, int height){
 		for (int i=0; i<fishPositions.length;i++){
 			fishPositions[i][1] = ((fishPositions[i][1] + 1)>=width)?
@@ -128,4 +171,22 @@ public class Main {
 		}
 	}
 
+	public static void moveAllObjects(int[][] positions, int dx, int dy, int width, int height){
+		int temp_x, temp_y;
+		for (int i=0; i<positions.length;i++){
+			temp_y = positions[i][1] + dy;
+			temp_x = positions[i][0] + dx;
+			while(temp_x < 0)
+				temp_x = temp_x + width;
+			while(temp_y < 0)
+				temp_y = temp_y + height;
+			positions[i][1] = ((temp_y)>=height)?
+									((temp_y)%height):
+									(temp_y);
+
+			positions[i][0] = ((temp_x)>=width)?
+									((temp_x)%width):
+									(temp_x);
+		}
+	}
 }
